e8e152caba9595dc1cea66ba9a58c6a86044bc17
Richard-AC <richard_abou_chaaya@brown.edu>
2021-12-14 21:24:31 +0100

    Commutative ring

--- src/project/eudoxus.lean
+++ src/project/eudoxus.lean
@@ -10,7 +10,10 @@ Reference: https://arxiv.org/pdf/math/0405454.pdf -/
   the set {f(m+n) - f(m) - f(n), n m : ℤ} is finite.
 
   Eudoxus Reals are then defined as equivalence classes of almost homomorphisms.
+  With f ~ g ↔ {f(n) - g(n), n : ℤ} is finite.
 
+  Therefore these are fonctions that grow almost linearily and the growth rate
+  represents a given real.
   For intuition, the Eudoxus real that represents the real number α will be the 
   equivalence class of (λ n : ℤ, ⌊α * n⌋).
 
